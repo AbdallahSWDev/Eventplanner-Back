4c99ad70cb9262aa4bcac25c448ac117ccd8b793
Abdallah Mohamed <91378659+AbdallahSWDev@users.noreply.github.com>
2025-12-28 20:13:49 +0200

    Update cors.go

--- cors.go
+++ cors.go
@@ -1,13 +1,16 @@
-package main
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-// CORSMiddleware allows the Angular frontend (localhost:4200) to access backend APIs
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		origin := c.Request.Header.Get("Origin")
+
+		allowedOrigins := map[string]bool{
+			"http://localhost:4200": true,
+			"https://eventplanner-front-azzohry-dev.apps.rm2.thpm.p1.openshiftapps.com": true,
+		}
+
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
