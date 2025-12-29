d0866a719e506f78b07c2fab073ce64e8c9ce8a0
Abdallah Mohamed <91378659+AbdallahSWDev@users.noreply.github.com>
2025-12-29 02:29:23 +0200

    Update cors.go

--- cors.go
+++ cors.go
@@ -14,12 +14,12 @@ func CORSMiddleware() gin.HandlerFunc {
 		}
 
 		if allowedOrigins[origin] {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Origin", origin)
 		}
 
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
